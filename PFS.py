0c40ce058c4c44ba7435adb7d0bcfeb9543ebf26
itsteele <itsteele@qu.edu>
2023-10-01 15:13:54 -0400

    Simple testing with reading strings from a file.

--- PFS.py
+++ PFS.py
@@ -1,2 +1,17 @@
 # Isaac Steele | Password Forensics Scanner
 
+# Potentially useful libraries : os, glob
+
+
+# Function that searches the string 
+def fileSearch(path, keyword):
+	with open(path, 'r') as file:
+		
+		data = file.read()
+	
+		if keyword in data:
+			print('unsecured password found!')
+		else:
+			print('no password vulnerabilities found')
+
+fileSearch(r'/Users/isaacsteele/Desktop/PFS-Test/private.txt', 'passwords')
