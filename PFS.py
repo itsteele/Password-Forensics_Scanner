5eea1fe9bfb4341d4475638908551882e1f57d29
itsteele <itsteele@qu.edu>
2023-12-19 13:15:06 -0500

    Updated algorithms and functions to search files.

--- PFS.py
+++ PFS.py
@@ -1,17 +1,26 @@
 # Isaac Steele | Password Forensics Scanner
 
-# Potentially useful libraries : os, glob
+import os
+import re
 
+def scan(directory):
+	for root, dirs, files in os.walk(directory):
+		for file in files:
+			file_path = os.path.join(root, file)
+			
+			# check file extensions
+			if re.search(r'\.(pwd|pw|pswd)$', file, re.IGNORECASE):
+				print(f"Potential password vulnerability found in file: {file_path}")
+			
+			# check file names and content for keywords
+			if re.search(r'password|pwd|private|personal|pass|passkey', file, re.IGNORECASE):
+				print(f"Potential password-related vulnerability found: {file_path}")
+			
+			# read file contents for potential passwords
+			with open(file_path, 'r') as f:
+				content = f.read()
+				if re.search(r'password|pwd|private|personal|pass|passkey', content, re.IGNORECASE):
+					print(f"Potential password-related vulnerability found: {file_path}")
 
-# Function that searches the string 
-def fileSearch(path, keyword):
-	with open(path, 'r') as file:
-		
-		data = file.read()
-	
-		if keyword in data:
-			print('unsecured password found!')
-		else:
-			print('no password vulnerabilities found')
-
-fileSearch(r'/Users/isaacsteele/Desktop/PFS-Test/private.txt', 'passwords')
+# run scanner
+scan('~/Users/isaacsteele/Desktop')
